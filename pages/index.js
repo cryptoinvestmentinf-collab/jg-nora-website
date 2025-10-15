850d66e1db6457038e93dceecf6cad6dce407f99
cryptoinvestmentinf-collab <cryptoinvestmentinf@gmail.com>
2025-10-15 11:25:23 +0800

    Create landing page index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the JG Nora Website</h1>
+            <p>Your landing page content goes here.</p>
+        </div>
+    );
+};
+
+export default HomePage;
